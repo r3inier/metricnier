5fd18e3c2116eb0b82b75f3bccaa257d52db47c7
r3inier <reinierdeleon@gmail.com>
2024-07-01 12:09:29 +0000

    edited lambda function to store json into s3

--- python/lambdas/hello-python/index.py
+++ python/lambdas/hello-python/index.py
@@ -1,5 +1,16 @@
+import json
+import boto3
+
 def lambda_handler(event, context):
-   return {
-       'message' : "TESTING HELLO WORLD!!!!",
-       'event': event
+    bucket_name = "metricnier-bucket"
+    
+    s3 = boto3.resource('s3')
+    s3_object = s3.Object(bucket_name, 'test-name.json')
+    s3_object.put(
+        Body=(bytes(json.dumps(event).encode('UTF-8')))
+    )
+
+    return {
+       'statusCode' : 200,
+       'body': 'Data uploaded successfully'
    }
