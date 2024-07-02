610dc0f29ba1a92e18f76ea6c5a2a3bc8ed70baf
r3inier <reinierdeleon@gmail.com>
2024-07-02 04:14:31 +0000

    created spotify API endpoint and lambda function

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 provider "aws" {
   region = "ap-southeast-2"
 }
-
+################################################################################################################################################################################
+# Permissions
 # Creating IAM role 
 resource "aws_iam_role" "lambda_role" {
 name   = "Lambda_Role"
@@ -40,10 +41,20 @@ resource "aws_iam_policy" "lambda_s3_policy" {
   })
 }
 
-resource "aws_lambda_permission" "apigw_lambda" {
+# Lambda permissions
+resource "aws_lambda_permission" "apigw_lambda_health" {
   statement_id = "AllowExecutionFromAPIGateway"
   action = "lambda:InvokeFunction"
   function_name = aws_lambda_function.store_health_data.function_name
+  principal = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.api_metricnier.execution_arn}/*"
+}
+
+resource "aws_lambda_permission" "apigw_lambda_spotify" {
+  statement_id = "AllowExecutionFromAPIGateway"
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.store_spotify_data.function_name
   principal = "apigateway.amazonaws.com"
 
   source_arn = "${aws_api_gateway_rest_api.api_metricnier.execution_arn}/*"
@@ -62,7 +73,7 @@ resource "aws_iam_role_policy_attachment" "lambda_s3_policy_attachment" {
 
 ################################################################################################################################################################################
 # Lambda functions
-# 
+# Store health data
 resource "aws_lambda_function" "store_health_data" {
 filename                       = "${path.module}/python/zips/store_health_data.zip"
 function_name                  = "store_health_data"
@@ -72,8 +83,19 @@ runtime                        = "python3.11"
 depends_on                     = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
 }
 
+# Store Spotify data
+resource "aws_lambda_function" "store_spotify_data" {
+filename                       = "${path.module}/python/zips/store_spotify_data.zip"
+function_name                  = "store_spotify_data"
+role                           = aws_iam_role.lambda_role.arn
+handler                        = "index.lambda_handler"
+runtime                        = "python3.11"
+depends_on                     = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
+}
+
 ################################################################################################################################################################################
-# Creating API key
+# API Gateway
+# Creating API keys
 resource "aws_api_gateway_api_key" "api_key" {
   name        = "metricnier_api_key"
   description = "Metricnier API Key"
@@ -97,7 +119,124 @@ resource "aws_api_gateway_usage_plan_key" "api_key_plan" {
   usage_plan_id = aws_api_gateway_usage_plan.usage_key.id
 }
 
-################################################################################################################################################################################
+# API & Lambda Health Integration 
+resource "aws_api_gateway_resource" "ingest_health" {
+  rest_api_id = aws_api_gateway_rest_api.api_metricnier.id
+  parent_id = aws_api_gateway_rest_api.api_metricnier.root_resource_id
+  path_part = "ingest-health"
+}
+
+resource "aws_api_gateway_method" "ingest_health" {
+  rest_api_id = aws_api_gateway_rest_api.api_metricnier.id
+  resource_id = aws_api_gateway_resource.ingest_health.id
+  http_method = "ANY"
+  authorization = "NONE"
+  api_key_required = true
+
+  request_parameters = {
+    "method.request.header.x-api-key" = true
+  }
+}
+
+resource "aws_api_gateway_method_response" "ingest_health" {
+  rest_api_id = aws_api_gateway_rest_api.api_metricnier.id
+  resource_id = aws_api_gateway_resource.ingest_health.id
+  http_method = aws_api_gateway_method.ingest_health.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "ingest_health" {
+  rest_api_id = aws_api_gateway_rest_api.api_metricnier.id
+  resource_id = aws_api_gateway_resource.ingest_health.id
+  http_method = aws_api_gateway_method.ingest_health.http_method
+  status_code = aws_api_gateway_method_response.ingest_health.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" =  "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
+
+  depends_on = [
+    aws_api_gateway_method.ingest_health,
+    aws_api_gateway_integration.lambda_health_integration
+  ]
+}
+
+resource "aws_api_gateway_integration" "lambda_health_integration" {
+  rest_api_id = aws_api_gateway_rest_api.api_metricnier.id
+  resource_id = aws_api_gateway_resource.ingest_health.id
+  http_method = aws_api_gateway_method.ingest_health.http_method
+  integration_http_method = "ANY"
+  type = "AWS"
+  uri = aws_lambda_function.store_health_data.invoke_arn
+}
+
+# API & Lambda Spotify Integration 
+resource "aws_api_gateway_resource" "ingest_spotify" {
+  rest_api_id = aws_api_gateway_rest_api.api_metricnier.id
+  parent_id = aws_api_gateway_rest_api.api_metricnier.root_resource_id
+  path_part = "ingest-spotify"
+}
+
+resource "aws_api_gateway_method" "ingest_spotify" {
+  rest_api_id = aws_api_gateway_rest_api.api_metricnier.id
+  resource_id = aws_api_gateway_resource.ingest_spotify.id
+  http_method = "ANY"
+  authorization = "NONE"
+  api_key_required = true
+
+  request_parameters = {
+    "method.request.header.x-api-key" = true
+  }
+}
+
+resource "aws_api_gateway_method_response" "ingest_spotify" {
+  rest_api_id = aws_api_gateway_rest_api.api_metricnier.id
+  resource_id = aws_api_gateway_resource.ingest_spotify.id
+  http_method = aws_api_gateway_method.ingest_spotify.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "ingest_spotify" {
+  rest_api_id = aws_api_gateway_rest_api.api_metricnier.id
+  resource_id = aws_api_gateway_resource.ingest_spotify.id
+  http_method = aws_api_gateway_method.ingest_spotify.http_method
+  status_code = aws_api_gateway_method_response.ingest_spotify.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" =  "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
+
+  depends_on = [
+    aws_api_gateway_method.ingest_spotify,
+    aws_api_gateway_integration.lambda_spotify_integration
+  ]
+}
+
+resource "aws_api_gateway_integration" "lambda_spotify_integration" {
+  rest_api_id = aws_api_gateway_rest_api.api_metricnier.id
+  resource_id = aws_api_gateway_resource.ingest_spotify.id
+  http_method = aws_api_gateway_method.ingest_spotify.http_method
+  integration_http_method = "ANY"
+  type = "AWS"
+  uri = aws_lambda_function.store_spotify_data.invoke_arn
+}
+
 # Creating API Gateway
 resource "aws_api_gateway_rest_api" "api_metricnier" {
   name = "api_metricnier"
@@ -108,73 +247,18 @@ resource "aws_api_gateway_rest_api" "api_metricnier" {
   }
 }
 
-resource "aws_api_gateway_resource" "root" {
-  rest_api_id = aws_api_gateway_rest_api.api_metricnier.id
-  parent_id = aws_api_gateway_rest_api.api_metricnier.root_resource_id
-  path_part = "health-export"
-}
-
-resource "aws_api_gateway_method" "health_export_method" {
-  rest_api_id = aws_api_gateway_rest_api.api_metricnier.id
-  resource_id = aws_api_gateway_resource.root.id
-  http_method = "ANY"
-  authorization = "NONE"
-  api_key_required = true
-
-  request_parameters = {
-    "method.request.header.x-api-key" = true
-  }
-}
-
-resource "aws_api_gateway_integration" "lambda_integration" {
-  rest_api_id = aws_api_gateway_rest_api.api_metricnier.id
-  resource_id = aws_api_gateway_resource.root.id
-  http_method = aws_api_gateway_method.health_export_method.http_method
-  integration_http_method = "ANY"
-  type = "AWS"
-  uri = aws_lambda_function.store_health_data.invoke_arn
-}
-
-resource "aws_api_gateway_method_response" "health_export_method" {
-  rest_api_id = aws_api_gateway_rest_api.api_metricnier.id
-  resource_id = aws_api_gateway_resource.root.id
-  http_method = aws_api_gateway_method.health_export_method.http_method
-  status_code = "200"
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = true,
-    "method.response.header.Access-Control-Allow-Methods" = true,
-    "method.response.header.Access-Control-Allow-Origin" = true
-  }
-}
-
-resource "aws_api_gateway_integration_response" "health_export_method" {
-  rest_api_id = aws_api_gateway_rest_api.api_metricnier.id
-  resource_id = aws_api_gateway_resource.root.id
-  http_method = aws_api_gateway_method.health_export_method.http_method
-  status_code = aws_api_gateway_method_response.health_export_method.status_code
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" =  "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
-    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
-    "method.response.header.Access-Control-Allow-Origin" = "'*'"
-  }
-
-  depends_on = [
-    aws_api_gateway_method.health_export_method,
-    aws_api_gateway_integration.lambda_integration
-  ]
-}
-
+# Staging API Gateway
 resource "aws_api_gateway_stage" "stage" {
   deployment_id = aws_api_gateway_deployment.deployment.id
   rest_api_id   = aws_api_gateway_rest_api.api_metricnier.id
   stage_name    = "default"
 }
 
+# Deploying API Gateway
 resource "aws_api_gateway_deployment" "deployment" {
   depends_on = [
-    aws_api_gateway_integration.lambda_integration
+    aws_api_gateway_integration.lambda_health_integration,
+    aws_api_gateway_integration.lambda_spotify_integration
   ]
 
   rest_api_id = aws_api_gateway_rest_api.api_metricnier.id
@@ -197,8 +281,14 @@ resource "aws_s3_bucket_public_access_block" "s3_bucket" {
 
 ################################################################################################################################################################################
 # Data Sources
-data "archive_file" "lambda_package" {
+data "archive_file" "lambda_store_health_package" {
   type = "zip"
   source_file = "python/lambdas/store_health_data/index.py"
   output_path = "python/zips/store_health_data.zip"
+}
+
+data "archive_file" "lambda_store_spotify_package" {
+  type = "zip"
+  source_file = "python/lambdas/store_spotify_data/index.py"
+  output_path = "python/zips/store_spotify_data.zip"
 }
