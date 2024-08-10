92af5e480a13677d1baa774d4898a660fd8e4599
r3inier <reinierdeleon@gmail.com>
2024-08-10 13:29:53 +0000

    blocked public access for s3 bucket

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,7 +1,3 @@
-resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "metricnier-bucket"
-}
-
 resource "aws_s3_bucket_public_access_block" "s3_bucket" {
   bucket = aws_s3_bucket.s3_bucket.id
 
@@ -9,6 +5,20 @@ resource "aws_s3_bucket_public_access_block" "s3_bucket" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_ownership_controls" "s3_bucket" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "s3_bucket" {
+  depends_on = [aws_s3_bucket_ownership_controls.s3_bucket]
+
+  bucket = aws_s3_bucket.s3_bucket.id
+  acl    = "private"
 }
 
 resource "aws_s3_bucket_notification" "trigger_health_lambdas" {
